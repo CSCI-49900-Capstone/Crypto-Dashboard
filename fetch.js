77c8ecc57a2dd68297a11e47569b4ac41d4a0d5b
ceonz <ceon91@gmail.com>
2023-11-26 22:39:16 -0500

    API connection and some design change

---
+++ fetch.js
@@ -0,0 +1,93 @@
+const API_URL = "https://feasible-mint-werewolf.ngrok-free.app"
+const AUTH_TOKEN = "AUTH_TOKEN"
+
+async function request(path, options = {}) {
+  path = path.startsWith('/') ? path : `/${path}`
+  token = localStorage.getItem(AUTH_TOKEN)
+  const result = await fetch(`${API_URL}${path}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...(token && { 'Authorization': `Bearer ${token}` })
+    },
+    ...options,
+  }).then((response) => response.json());
+  if (!result?.ok) {
+    console.error(result?.error ?? result)
+  }
+  return result
+}
+
+const api = {
+  signUp(input = {}) {
+    return request('/sign-up', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: input.email,
+        password: input.password,
+        api_key: input.apiKey,
+        secret_key: input.secretKey,
+        account: input.account,
+      }),
+    })
+  },
+  async signIn(input = {}) {
+    const result = await request('/sign-in', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: input.email,
+        password: input.password,
+      }),
+    })
+    if (result?.data?.token) {
+      localStorage.setItem(AUTH_TOKEN, result.token);
+    }
+    return result
+  },
+  async getProfile() {
+    return request('/profile', {
+      method: 'GET',
+    })
+  },
+  async updateProfile(input = {}) {
+    return request('/profile', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        password: input.password,
+        api_key: input.apiKey,
+        secret_key: input.secretKey,
+        account: input.account,
+      }),
+    })
+  },
+  async deleteProfile() {
+    const result = await request('/profile', {
+      method: 'DELETE',
+    })
+    if (result?.ok === true) {
+      localStorage.removeItem(AUTH_TOKEN);
+    }
+  },
+  async enableAutoTrading(input = {}) {
+    return await request('/auto-trade/enable', {
+      method: 'PATCH',
+      body: JSON.stringify({
+        k: input.k, // 0.1 ~ 1.0
+        ratio: input.ratio, // 0.10 ~ 1.00
+        ticker: input.ticker, // BTC-USD
+      }),
+    })
+  },
+  async disableAuthTrading(ticker) {
+    return await request('/auto-trade/disable', {
+      method: 'PATCH',
+      body: JSON.stringify({ 
+        ticker: ticker, // BTC-USD
+      }),
+    })
+  },
+  async getTransactions(ticker) {
+    return request(`/transactions?=${ticker}`, {
+      method: 'GET',
+    })
+  },
+}
--- login.js
+++ login.js
@@ -1,7 +1,8 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-
+const loginForm  = document.querySelector('#js-login-form');
+const registerForm  = document.querySelector('#js-register-form');
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -10,3 +11,45 @@ registerLink.addEventListener('click', ()=> {
 loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
+
+loginForm.addEventListener('submit', async (event)=> {
+  event.preventDefault();
+  form = event.target;
+  const email = form.querySelector('input[name="email"]');
+  const password = form.querySelector('input[name="password"]');
+
+  const result = await api.signIn({
+    email: email.value,
+    password: password.value,
+  })
+  if (result.ok) {
+    window.location.href = '/';
+  } else {
+    alert(result.error);
+  }
+})
+
+registerForm.addEventListener('submit', async (event)=> {
+  event.preventDefault();
+  form = event.target;
+  const email = form.querySelector('input[name="email"]');
+  const password = form.querySelector('input[name="password"]');
+  const apiKey = form.querySelector('input[name="api_key"]');
+  const secretKey = form.querySelector('input[name="secret_key"]');
+  const account = form.querySelector('input[name="account"]');
+
+  const result = await api.signUp({
+    email: email.value,
+    password: password.value,
+    apiKey: apiKey.value,
+    secretKey: secretKey.value,
+    account: account.value,
+  })
+  if (result.ok) {
+    wrapper.classList.remove('active');
+    const loginEmail = loginForm.querySelector('input[name="email"]');
+    loginEmail.value = email.value;
+  } else {
+    alert(result.error);
+  }
+})
